682b69ffc5c2312796fd2fe4465d27c97ada7a0b
vagrant <vagrant@localhost.localdomain>
2020-08-17 17:07:34 +0000

    [Finish] 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)   
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :index
@@ -39,7 +39,6 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
